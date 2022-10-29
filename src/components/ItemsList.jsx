a4f69d83ba42b57a6adc7d363cc54d2ba2147944
nelfimov <nelfimov@mail.ru>
2022-10-29 14:42:43 +0300

    further styling

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import items from '../assets/items.json';
+import propTypes from 'prop-types';
 import '../styles/ItemsList.css';
 
 const ITEMS_LIST = items.items;
 
-const ItemsList = () => {
+const ItemsList = (props) => {
+  const {addToCard} = props;
+
   return (
     <div className='items-container'>
       {ITEMS_LIST.map((item) => (
-        <div className='item-card' key={item.id}>
+        <div className='item-card' key={item.id} onClick={addToCard}>
           <p>{item.name}</p>
           <p>${item.price}</p>
         </div>
       ))}
     </div>
   );
+};
+
+ItemsList.propTypes = {
+  addToCard: propTypes.func.isRequired,
 };
 
 export default ItemsList;
